19e8f5a545ecf01f19b43516f3a4958d3d6bba54
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-03-21 22:28:59 +0400

    cc

--- game/index.js
+++ game/index.js
@@ -1,50 +1,103 @@
-var container = document.querySelector(".container");
-var eachBox = document.querySelectorAll(".each-box");
-var choosen = document.querySelector(".choosen");
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
-const colorList = [
-  "#7149C6",
-  "#FC2947",
-  "#FE6244",
-  "#245953",
-  "#408E91",
-  "#E49393",
-];
+var container = document.querySelector(".container");
+var result = document.querySelector(".result")
+
+const arr = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+]
+
+class Peace {
+    constructor(xCor, yCor, color) {
+        this.xCor = xCor
+        this.yCor = yCor
+        this.color = color
+        this.chosedItems = []
+        this.eachPeace = document.createElement("div")
+        this.markedColors = []
+    }
+
+    drawElement() {
+        this.eachPeace.classList.add("item");
+        this.eachPeace.classList.add(this.color)
+        container.appendChild(this.eachPeace)
+
+        this.giveStyles();
+
+        this.listener()
+    }
+    giveStyles() {
+        this.eachPeace.style.top = `${this.xCor}px`;
+        this.eachPeace.style.left = `${this.yCor}px`;
+        this.eachPeace.style.background = this.color
+    }
+    listener() {
+
+        this.eachPeace.addEventListener("click", (e) => {
+            var currentItem = e.currentTarget
+            // var colors = ["#7149C6", "#FC2947", "#FE6244", "#245953", "#408E91", "#EA5455", "#002B5B"]
+            if (result.querySelectorAll(".item").length <= 5) {
+                currentItem.style.position = "static"
+                currentItem.classList.add("choosed")
+                result.append(currentItem)
+            }
+            var choosedElements = Array.from(document.querySelectorAll(".choosed"))
+
+            switch (currentItem.classList[1]) {
+                case "#7149C6":
+                    arr[0].push("#7149C6");
+                    break
+                case "#FC2947":
+                    arr[1].push("#FC2947");
+                    break
+                case "#FE6244":
+                    arr[2].push("#FE6244");
+                    break
+                case "#245953":
+                    arr[3].push("#245953");
+                    break
+                case "#408E91":
+                    arr[4].push("#408E91");
+                    break
+                case "#EA5455":
+                    arr[5].push("#EA5455");
+                    break
+                case "#002B5B":
+                    arr[6].push("#002B5B");
+                    break;
+            }
+
+            arr.forEach((ee)=> {
+                if (ee.length >= 2) {
+                    ee = []
+                }
+            })
+
+        })
+    }
+}
 
 window.addEventListener("load", () => {
-  const element = {
-    top: Math.random() * 500,
-    left: Math.random() * 500,
-  };
-  const row = colorList.length.toString();
-
-  for (var i = 0; i < 50; i++) {
-    const eachBoxElement = document.createElement("div");
-    eachBoxElement.classList.add("each-box");
-    eachBoxElement.style.left = `${randomNumber(0, 430)}px`;
-    eachBoxElement.style.top = `${randomNumber(0, 430)}px`;
-    eachBoxElement.style.zIndex = Math.floor(randomNumber(0, colorList.length));
-    if (eachBoxElement.style.zIndex == "5") {
-      eachBoxElement.style.back = "gray";
+    function drawDifferent(res, c) {
+        for (var i = 0; i < res; i++) {
+            var eachBox = new Peace(
+                getRandomArbitrary(0, 500),
+                getRandomArbitrary(0, 500),
+                c
+            )
+            eachBox.drawElement();
+        }
     }
-    eachBoxElement.style.background =
-      colorList[Math.floor(Math.random() * colorList.length)];
+    var colors = ["#7149C6", "#FC2947", "#FE6244", "#245953", "#408E91", "#EA5455", "#002B5B"]
+    colors.map((each) => {
+        drawDifferent(2, each)
+    })
 
-    if (eachBoxElement.style.zIndex === row) {
-      eachBoxElement.style.background = "gray";
-    }
-    container.appendChild(eachBoxElement);
-    eachBoxElement.addEventListener("click", (e) => {
-      if (e.currentTarget.style.zIndex === "5") {
-        e.currentTarget.style.position = "static";
-        e.currentTarget.classList.add("choosen-box");
-        choosen.appendChild(e.currentTarget);
-        // console.log((e.currentTarget.style.background = "gray"));
-      }
-      const allChosenBox = document.querySelectorAll(".choosen-box");
-    });
-  }
-});
-3320005133;
+})
